40776c5fa2c5d617f34599c53e3ce35c36327a48
keldendraduldorji <kelden.dorji@6dtech.co.in>
2020-05-12 21:49:53 +0530

    REact dont work

--- server/server.js
+++ server/server.js
@@ -3,12 +3,11 @@ const app = express()
 const path = require('path')
 
 const port = process.env.PORT || 5000
-const publicPath = path.join(__dirname, '..', 'build')
+const publicPath = path.join(__dirname, '..', 'public')
 
 app.use(express.static(publicPath))
 
 app.get('*', (req, res) => {
-    console.log("heooooo",path.join(publicPath, 'index.html'))
     res.sendFile(path.join(publicPath, 'index.html'));
  });
 
