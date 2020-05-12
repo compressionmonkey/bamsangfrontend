028f8f1bde4b49e7fd5331e90c5021baab0f2fcf
keldendraduldorji <kelden.dorji@6dtech.co.in>
2020-05-12 22:03:51 +0530

    hey

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require('path')
 
 const port = process.env.PORT || 5000
-const publicPath = path.join(__dirname, '..', 'public')
+const publicPath = path.join(__dirname, '..', 'build')
 
 app.use(express.static(publicPath))
 
