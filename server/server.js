2ba0c11e909817269dd1d077045a0225292b50c0
keldendraduldorji <kelden.dorji@6dtech.co.in>
2020-05-15 03:34:50 +0530

    added popup and webcam

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ const publicPath = path.join(__dirname, '..', 'build')
 app.use(express.static(publicPath))
 
 app.get('*', (req, res) => {
-    console.log('heyyy')
     res.sendFile(path.join(publicPath, 'index.html'));
  });
 
--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import React from 'react';
-
+import React, { useRef, useCallback, useState } from 'react';
+import Webcam from "react-webcam"
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
-   function logIn(){
+  const [clicked, setclicked] = useState(false)
+  const [show, setShow] = useState(false)
+
+  const handleClose = () => {
+    setShow(false)
+  }
+
+  const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: "user"
+  }
+
+  const webcamRef = useRef(null);
+   
+  const capture = useCallback(
+    () => {
+      const imageSrc = webcamRef.current.getScreenshot();
+      console.log(imageSrc)
+    },
+    [webcamRef]
+  )
+   const logIn = () => {
+    setShow(true)
     fetch('https://bamsangbackend.herokuapp.com/login', 
     {
       mode: 'no-cors'
@@ -18,6 +44,29 @@ function App() {
           left: '500px',
           top: '300px'}}
           onClick={logIn}>Log in</button>
+
+          <Modal size="lg" show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Please take a photo</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+            <Webcam
+        audio={false}
+        height={550}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={750}
+        videoConstraints={videoConstraints}
+      />
+            </Modal.Body>
+
+            <Modal.Footer>
+              <Button onClick={ handleClose } variant="secondary">Close</Button>
+              <Button onClick={capture} variant="primary">Save changes</Button>
+            </Modal.Footer>
+          </Modal>
+      {/* <button onClick={capture}>Capture photo</button> */}
     </div>
   );
 }
