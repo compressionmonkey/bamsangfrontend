3b4cb24974df2851d98d7e7afa01e73e4e6a35b6
keldendraduldorji <kelden.dorji@6dtech.co.in>
2020-06-17 16:54:19 +0530

    added media queries for mobile view

--- src/App.js
+++ src/App.js
@@ -4,59 +4,17 @@ import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import axios from 'axios'
-import { Line } from 'react-chartjs-2'
+import LoggedIn from './LoggedIn'
+
 // import imagepath from 'https://unsplash.com/photos/8n9E1ZO7F80'
 function App() {
   const [show, setShow] = useState(false)
   const [graph, setGraph] = useState(false)
 
-  const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }, {
-            label: '# of Names',
-            data: [7, 10, 5, 3, 8, 4],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(54, 162, 235, 1)'
-            ],
-            borderWidth: 1
-        }]
-  }
-
   const handleClose = () => {
     setShow(false)
   }
 
-  const predictprices = () => {
-    axios.get('https://bamsangai.herokuapp.com/priceprediction')
-  }
 
   const videoConstraints = {
     width: 1280,
@@ -94,8 +52,8 @@ function App() {
     setShow(true)
   }
   return (
-    <div style={{background:"black"}}>
-        <section class="animated-grid">
+    <div>
+        {!graph ? <section class="animated-grid">
           <div class="card" style={{background:"rgb(36,243, 147)"}}>1</div>
           <div class="card" style={{background:"rgb(36,243, 147)"}}>2</div>
           <div class="card" style={{background:"rgb(36,243, 147)"}}>3</div>
@@ -109,13 +67,8 @@ function App() {
           <div class="card" style={{background:"rgb(36,243, 147)"}}>11</div>
           <div class="card" style={{background:"rgb(36,243, 147)"}}>12</div>
           <div class="card" style={{background:"rgb(36,243, 147)"}}>13</div>
-
-        </section>
-        {!graph && <button style={{
-          position: 'relative',
-          left: '500px',
-          top: '300px'}}
-          onClick={logIn}>Log in</button>}
+        
+          <button onClick={logIn}>Log in</button>
 
           <Modal size="lg" show={show} onHide={handleClose}>
             <Modal.Header closeButton>
@@ -138,19 +91,7 @@ function App() {
               <Button onClick={capture} variant="primary">Take a snap</Button>
             </Modal.Footer>
           </Modal>
-          {
-            graph && <Line
-              data = {data}
-              width = {100}
-              height = {200}
-              options = {{
-                maintainAspectRatio: false
-              }} 
-            />
-          }
-          {
-            graph && predictprices()
-          }
+        </section>:<LoggedIn/>}
     </div>
   );
 }
